a25f9c572b198fe761877245d5dde54c3d3dac9b
Mashuda Siddika Maisha <137620245+mashudasiddikamaisha@users.noreply.github.com>
2024-11-25 23:33:28 +0600

    Create 6. DFS.cpp

---
+++ 6. DFS.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> v[1005];
+bool vis[1005];
+
+void dfs(int src)
+{
+  cout<<src<<endl;
+  vis[src] = true;
+  for(int child : v[src])
+  {
+    if(!vis[child])
+    {
+      dfs(child);
+    }
+  }
+  
+}
+int main() 
+{
+    int n,e;
+    cin>>n>>e;
+    while(e--)
+    {
+      int a,b;
+      cin>>a>>b;
+      v[a].push_back(b);
+      v[b].push_back(a);
+    }
+    memset(vis, false, sizeof(vis));
+    dfs(0);
+    return 0;
+}
